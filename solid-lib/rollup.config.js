441f9f3ee2e6458a1d24b3be9470d4642c49663d
lingj <lingj.jhf@outlook.com>
2023-02-09 16:45:17 +0800

    修改模板

---
+++ solid-lib/rollup.config.js
@@ -0,0 +1,3 @@
+import { defineConfig } from 'rollup'
+
+export default defineConfig({ input: './src/index.tsx' })
---
+++ solid-lib/src/index.tsx
@@ -0,0 +1,5 @@
+import { render } from 'solid-js/web'
+
+const root = document.getElementById('root')
+
+render(() => <div>Hello World</div>, root as HTMLElement)
